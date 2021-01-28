9ec56bd8a327591053721bbb9e8d3bd051c760e8
ytyaru <yttry0@gmail.com>
2021-01-28 11:44:04 +0900

    clean

--- src/my_project/src/App.vue
+++ src/my_project/src/App.vue
@@ -1,25 +1,10 @@
 <template>
   <el-button type="primary" @click="onClick">Element UI のボタンだよ</el-button>
-  <el-date-time-picker></el-date-time-picker>
-  <el-date-picker 
-        type="daterange" 
-        v-model="value1" 
-        range-separator="〜" 
-        start-placeholder="開始日" 
-        end-placeholder="終了日">                            
-  </el-date-picker>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  },
   setup() {
     const onClick = ()=>{
       alert('よくも押してくれたな！');
@@ -31,13 +16,3 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
